729ef214e78d99a1f7dcd133bcda8755392bfa91
Linus Johansson <linus97johansson@gmail.com>
2024-10-31 18:15:06 +0100

    Added next.js:15 and removed all the boilerplate.

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+       {/* Too be added */}
+       <h1>test</h1>
+      </main>
+      <footer>
+       {/* Too be added */}
+      </footer>
+    </>
+  );
+}
